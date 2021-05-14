7ac5db44b4c450ab941c79baf9d36dac32cc08ec
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2021-05-14 12:06:26 +0200

    first commit

---
+++ src/main/kotlin/at/fhj/ima/lazyrecipes/LazyrecipesApplication.kt
@@ -0,0 +1,11 @@
+package at.fhj.ima.lazyrecipes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LazyrecipesApplication
+
+fun main(args: Array<String>) {
+	runApplication<LazyrecipesApplication>(*args)
+}
---
+++ src/test/kotlin/at/fhj/ima/lazyrecipes/LazyrecipesApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.fhj.ima.lazyrecipes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LazyrecipesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
